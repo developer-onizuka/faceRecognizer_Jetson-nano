1fb6423652e9da1dbc02c6cd58f71929769816a2
developer-onizuka <67852921+developer-onizuka@users.noreply.github.com>
2021-08-24 22:09:06 +0900

    2021-08-24

--- test.py
+++ test.py
@@ -6,21 +6,15 @@ myKit=ServoKit(channels=16)
 #import time
 print(cv2.__version__)
 
-donFace=face_recognition.load_image_file('/home/kentaro/Desktop/pyPro/faceRecognizer/demoImages/known/Donald Trump.jpg')
+donFace=face_recognition.load_image_file('~/Desktop/pyPro/faceRecognizer/demoImages/known/Donald Trump.jpg')
 donEncode=face_recognition.face_encodings(donFace)[0]
 #print(donEncode)
-nancyFace=face_recognition.load_image_file('/home/kentaro/Desktop/pyPro/faceRecognizer/demoImages/known/Nancy Pelosi.jpg')
+nancyFace=face_recognition.load_image_file('~/Desktop/pyPro/faceRecognizer/demoImages/known/Nancy Pelosi.jpg')
 nancyEncode=face_recognition.face_encodings(nancyFace)[0]
 #print(nancyEncode)
-hisayukiFace=face_recognition.load_image_file('/home/kentaro/Desktop/pyPro/videos/hisayuki.jpg')
-hisayukiEncode=face_recognition.face_encodings(hisayukiFace)[0]
-#print(hisayukiEncode)
-kentaroFace=face_recognition.load_image_file('/home/kentaro/Desktop/pyPro/videos/kentaro.jpg')
-kentaroEncode=face_recognition.face_encodings(kentaroFace)[0]
-#print(kentaroEncode)
 
-Encodings=[donEncode,nancyEncode,kentaroEncode]
-Names=['The Donald','Nancy Pelosi','Kentaro']
+Encodings=[donEncode,nancyEncode]
+Names=['The Donald','Nancy Pelosi']
 
 font=cv2.FONT_HERSHEY_SIMPLEX
 dispW=640
@@ -31,8 +25,6 @@ cam=cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FPS,30)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, dispW)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, dispH)
-
-#face_cascade=cv2.CascadeClassifier('/home/kentaro/Desktop/pyPro/cascade/face.xml')
 
 CenterW1=int(dispW/2)-int(dispW*.01)
 CenterW2=int(dispW/2)+int(dispW*.01)
@@ -49,14 +41,11 @@ while True:
     _,frame=cam.read()
     frameSmall=cv2.resize(frame,(0,0),fx=.5,fy=.5)
     frameRGB=cv2.cvtColor(frameSmall,cv2.COLOR_BGR2RGB)
-    #gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     facePositions=face_recognition.face_locations(frameRGB,model='cnn')
-    #facePositions=face_recognition.face_locations(frameRGB)
     allEncodings=face_recognition.face_encodings(frameRGB,facePositions)
     for (top,right,bottom,left),face_encoding in zip(facePositions,allEncodings):
         name='Unknown Person'
         matches=face_recognition.compare_faces(Encodings,face_encoding)
-        #faces=face_cascade.detectMultiScale(gray,1.3,5)
         if True in matches:
             first_match_index=matches.index(True)
             name=Names[first_match_index]
@@ -64,7 +53,7 @@ while True:
         right=int(right*2)
         bottom=int(bottom*2)
         left=int(left*2)
-        if matches[2]:
+        if matches[0]:
             red=255
         cv2.rectangle(frame,(left,top),(right,bottom),(blue,green,red),2)
         cv2.putText(frame,name,(left,top-6),font,.75,(0,255,255),1)
